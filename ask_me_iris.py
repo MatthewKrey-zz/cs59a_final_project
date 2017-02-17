42ff96a691e540ef28bbad7f5fb31a97d9b10e8c
MatthewKrey <kreyenglish@gmail.com>
2017-02-17 02:25:13 -0800

    add intro logic. add welcome. add prompts. add Iris decision tree heuristics

--- ask_me_iris.py
+++ ask_me_iris.py
@@ -1,17 +1,20 @@
-# Import Iris Data Set
+# Import Dependent Libraries & Iris Data Set
+
 import sklearn
-from sklearn.datasets import load_iris
-
-iris = load_iris()
-print iris.feature_names
-print iris.target_names
-
-# Train classifier
-
 import numpy as np
+import pydotplus
 from sklearn.datasets import load_iris
 from sklearn import tree
+from sklearn.externals.six import StringIO
+
+# Test data set
+
 iris = load_iris()
+# print iris.feature_names
+# print iris.target_names
+
+# Split Training & Test Data
+
 test_idx = [0,50,100]
 
 # training data
@@ -22,32 +25,38 @@ train_data = np.delete(iris.data, test_idx, axis=0)
 test_target = iris.target[test_idx]
 test_data = iris.data[test_idx]
 
-# import classifier and train on training data
+# Train Decision Tree Classifier on training data
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(train_data, train_target)
 
-print test_target
-print clf.predict(test_data)
+# print test_target
+# print clf.predict(test_data)
 
-# Visualize Decision Tree to see how classifier works
+# Begin Intro of Iris to User
 
-from sklearn.externals.six import StringIO
-import pydotplus
+print '''Hello, my name is Iris. I love data and I love to play games. \n
+Would you like to play a game about my favorite data set with me? \n
+1. No
+2. Yes
+3. Maybe \n'''
 
-dot_data = StringIO()
-tree.export_graphviz(clf,
-                        out_file = dot_data,
-                        feature_names = iris.feature_names,
-                        class_names = iris.target_names,
-                        filled = True,
-                        rounded = True,
-                        impurity = False)
+answer = raw_input("> ")
 
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-graph.write_pdf("iris.pdf")
+if answer == "1":
+    print "Okay. No worries. Perhaps another time."
 
-print test_data[0], test_target[0]
-print test_data[1], test_target[1]
+elif answer == "3":
+    print "Are you sure? It is SUPER fun! I love data. I think you will too. Want to join the fun? \n\n 1. No \n 2. Yes\n"
+    maybe_answer = raw_input("> ")
+    if maybe_answer == "1":
+        print "Alright grumpy pants. I'm going to go find some data sets!"
+    if maybe_answer == "2":
+        print "Right on! Let's play! :)"
 
-print iris.feature_names, iris.target_names
+elif answer == "2":
+    print "Right on! You are going to love this game."
+
+
+    # for i in range(len(iris.target)):
+    #     print "Example %d: label %s, features %s" % (i, iris.target[i], iris.data[i])
