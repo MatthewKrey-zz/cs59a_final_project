079146c7fe9bdd76341c667db8ef6fb2eb9f7f6a
MatthewKrey <kreyenglish@gmail.com>
2017-02-16 23:53:37 -0800

    push Documentation v1

--- ask_me_iris.py
+++ ask_me_iris.py
@@ -3,8 +3,8 @@ import sklearn
 from sklearn.datasets import load_iris
 
 iris = load_iris()
-# print iris.feature_names
-# print iris.target_names
+print iris.feature_names
+print iris.target_names
 
 # Train classifier
 
